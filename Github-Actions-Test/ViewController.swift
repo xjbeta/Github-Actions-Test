3648a34634e01d76bf7604654d90802477a9b5ba
xjbeta <xjbeta.tiny@gmail.com>
2019-12-07 19:36:33 +0800

    Initial Commit

---
+++ Github-Actions-Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Github-Actions-Test
+//
+//  Created by xjbeta on 2019/12/7.
+//  Copyright © 2019 xjbeta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
